ff9d1c3f3f5f70a83223ff8206a3170a8c7c39b0
Melvin Agustin <agustinarsenio122@gmail.com>
2022-08-23 11:38:58 +0800

    Add files via upload

--- js/showcaseSectionEffect.js
+++ js/showcaseSectionEffect.js
@@ -1,6 +1,9 @@
 const showcaseImg = document.querySelector('#laptop-for-desktop');
+const hireBtn = document.querySelector('#hire-btn');
 
 // image slide effect
 window.addEventListener('scroll', () => {
     showcaseImg.style.transform = `translateX(${window.scrollY}px)`;
 });
+
+hireBtn.addEventListener('click', () => window.open('../pages/hire-page.html'));
---
+++ pages/hire-page.js
@@ -0,0 +1,1 @@
+const headerText = document.querySelector('h1').addEventListener('click', () => (location.href = '../index.html'));
